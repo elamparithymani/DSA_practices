3c25a300c8f17d9b2aa36471095846e0fa615a63
elamparithymani <elamparithy888@gmail.com>
2025-12-24 23:47:25 +0530

    Initial Commit

---
+++ DSA_JavaPractices/src/BasicLogicBuildingPractices/positiveornegative.java
@@ -0,0 +1,25 @@
+package BasicLogicBuildingPractices;
+
+import java.util.Scanner;
+
+public class positiveornegative {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" positive, negative, or zero.");
+
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        System.out.println("Given Input  " + num);
+
+        if(num > 0) {
+            System.out.println("The Given input is positive");
+        }
+        else if(num<0) {
+            System.out.println("The Given Input is Negative");
+        }
+        else {
+            System.out.println("Zero Input");
+        }
+
+    }
+}
